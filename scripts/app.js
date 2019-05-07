fb01d2844a8e809c976be260a8ed4f390c93e383
TawshifTamjidAlam <tawshif27@gmail.com>
2019-05-06 22:02:06 -0700

    add user cards to page

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,32 @@
 "use strict"
 
 document.addEventListener('DOMContentLoaded', async () => {
-    console.log(await getRandomUsers());
+    addUsersToPage();
 });
+
+async function addUsersToPage(){
+    const users = await getRandomUsers();
+    const gallery = document.querySelector('#gallery');
+    users.forEach((user) => {
+        const card = createUserCardElement(user);
+        gallery.appendChild(card);
+    });
+}
+
+function createUserCardElement(user){
+    const card = document.createElement('div');
+       card.classList.add('card');
+       card.innerHTML = `<div class="card-img-container">
+       <img class="card-img" src=${user.picture.medium} alt="profile picture">
+   </div>
+   <div class="card-info-container">
+       <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+       <p class="card-text">${user.email}</p>
+       <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+   </div>`;
+
+   return card;
+}
 
 async function getRandomUsers (numberOfUsers = 12 , nationalities = ['us']){
     const concatedNationalities = nationalities.join();
